6ee57760f39d949391d92a5a74b9ff9928b8f125
Nicholas Ogden <nickogden91@gmail.com>
2014-03-15 20:31:26 -0700

    updated comments

--- Code/thermocouple.c
+++ Code/thermocouple.c
@@ -14,21 +14,44 @@
 #include "spi.h"
 #include "thermocouple.h"
 
-/*******************************************************************************
- *                          Global Variables
- ******************************************************************************/
-
 
 /*******************************************************************************
  *                             Other Functions
  ******************************************************************************/
 
+/**
+ *  \brief   Thermocouple initialization
+ *
+ *  This function initializes the PIC registers and SPI controller used
+ *  for communicating with the Maxim 31855 thermocouple amplifier
+ *
+ *  \param  None
+ *
+ *  \return None
+ *
+ *  \note   None
+ */
 void initThermocouple()
 {
     TRISC &= 0b10111111; // set C6 as output
     initSPI();
 }
 
+
+/**
+ *  \brief   Read thermocouple temperature
+ *
+ *  This function initializes the PIC registers and SPI controller used
+ *  for communicating with the Maxim 31855 thermocouple amplifier
+ *
+ *  \param  None
+ *
+ *  \return (unsigend int) temperature of the thermocouple or returns -1 if
+ *          the MAX31855 reports a thermocouple error.
+ *
+ *  \note   This function should not be called at a rate faster than 10Hz
+ *          because it takes 100ms for the MAX31855 to measure the temperature.
+ */
 unsigned int getTemp()
 {
     unsigned int t=0;
@@ -48,4 +71,3 @@ unsigned int getTemp()
 	else
 		return t; // otherwise return temperature value
 }
-
