61cbbe036dc6c0ce6bc0f8b0b21ed7342721acda
Nicholas Ogden <nickogden91@gmail.com>
2014-03-15 20:25:23 -0700

    updated comments

--- Code/spi.c
+++ Code/spi.c
@@ -18,6 +18,17 @@
  *                             Other Functions
  ******************************************************************************/
 
+/**
+ *  \brief  SPI initialization
+ *
+ *  This function initializes the variables used in the SPI controller
+ *
+ *  \param  None
+ *
+ *  \return None
+ *
+ *  \note   None
+ */
 void initSPI()
 {
     TRISC |= 0b00010000;  // set C4 as input
@@ -25,6 +36,20 @@ void initSPI()
     SSPCON =  0b00100010; // setup mssp
 }
 
+
+/**
+ *  \brief   SPI receive byte
+ *
+ *  This function reads a byte from the SPI line
+ *
+ *  \param  None
+ *
+ *  \return (byte) returns the byte read from the SPI line
+ *
+ *  \note   This function will not return until the entire byte has been read.
+ *          If the SPI clock is very slow, this could take a non-negligable
+ *          amount of time.
+ */
 unsigned char SPIReceive()
 {
     SSPBUF = 0;  // clock 8 bits
