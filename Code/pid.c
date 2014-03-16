60853d79d0172918aa8358b936db28c8946c48b6
Nicholas Ogden <nickogden91@gmail.com>
2014-03-15 20:22:07 -0700

    updated comments

--- Code/pid.c
+++ Code/pid.c
@@ -17,28 +17,45 @@
  *                          Global Variables
  ******************************************************************************/
 
+// coefficients for P,I, and D
 int Kp;
 int Ki;
 int Kd;
 
+// current values for P,I, and D
 int PVal;
 int IVal;
 int DVal;
 
-int previousVal;
-int setPoint;
+int previousVal; // previous value of PID input
+int setPoint;    // set point for PID controller
 
 
 /*******************************************************************************
  *                             Other Functions
  ******************************************************************************/
 
+/**
+ *  \brief   PID controller initialization
+ *
+ *  This function initializes the variables used in the PID controller
+ *
+ *  \param  (int) p: proportional coefficient (Kp)
+ *          (int) i: integral coefficient (Ki)
+ *          (int) d: derivative coefficient (Kd)
+ *
+ *  \return None
+ *
+ *  \note   None
+ */
 void initPID(int p, int i, int d)
 {
+    // initialize the coeficcients Kp, Ki, and Kd
     Kp = p;
     Ki = i;
     Kd = d;
 
+    // resets the P,I, and D values
     PVal = 0;
     IVal = 0;
     DVal = 0;
@@ -46,11 +63,37 @@ void initPID(int p, int i, int d)
     previousVal = 0;
 }
 
+
+/**
+ *  \brief   Update set point for PID controller
+ *
+ *  This function changes the set point for the PID controller
+ *
+ *  \param  (int) s: the new set point for the PID controller
+ *
+ *  \return None
+ *
+ *  \note   None
+ */
 void setPIDVal(int s)
 {
     setPoint = s;
 }
 
+
+/**
+ *  \brief   PID step
+ *
+ *  This function computes the next value for PID controller given the current
+ *  value and the elapsed time since the previous step
+ *
+ *  \param  (int) dt: elapsed time since last PID step
+ *          (int) currentVal: current value of system
+ *
+ *  \return None
+ *
+ *  \note   None
+ */
 int pidStep(int dt ,int currentVal)
 {
     int error;
